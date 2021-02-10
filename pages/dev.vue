5490b6f563533620a74dfaad6121e8fb67947233
SOKHUONG UON <uonsokhuong18@kit.edu.kh>
2021-02-10 13:51:48 +0700

    make this development page

---
+++ pages/dev.vue
@@ -0,0 +1,36 @@
+<template>
+<main class="w-screen h-screen">
+	<!-- Main Section -->
+	<div class="w-full h-full bg-gray-300 dark:bg-black flex overflow-hidden">
+		
+		<!-- Server Section -->
+		<section class=" w-14 h-full bg-green-400 flex-shrink-0"></section>
+
+		<!-- Channel Section -->
+		<section class=" w-56 h-full bg-teal-700 flex-shrink-0"></section>
+
+		<!-- Chat Section -->
+		<section class="flex-1 bg-gray-600"></section>
+	</div>
+</main>
+</template>
+
+<script>
+export default {
+
+	data() {
+		return {
+		
+		}
+	},
+
+	methods: {
+
+	},
+
+	mounted() {
+
+	}
+
+}
+</script>
