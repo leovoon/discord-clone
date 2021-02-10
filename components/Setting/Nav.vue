3e5c60013e28a91ca0d1b48a18706af7011885c0
SOKHUONG UON <uonsokhuong18@kit.edu.kh>
2021-02-10 14:23:59 +0700

    finish the main part of navigation

--- components/Setting/Nav.vue
+++ components/Setting/Nav.vue
@@ -4,33 +4,55 @@
 		<section class="w-full flex flex-col">
 			<p class="uppercase w-full h-7 pb-3">suer setting</p>
 			<div class="flex-1">
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">My Accoung</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Privacy & Safety</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Authorized Apps</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Connections</button>
 			</div>
 		</section>
+
+		<div class="divide-y-4 h-3 divide-solid divide-gray-900"></div>
 
 		<!-- billing setting -->
 		<section class="w-full flex flex-col">
 			<p class="uppercase w-full h-7 pb-3">billing setting</p>
 			<div class="flex-1">
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Discord Nitro</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Server Boost</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Gift Inventory</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Billing</button>
 			</div>
 		</section>
+
+		<div class="divide-y-4 h-3 divide-solid divide-gray-900"></div>
 
 		<!-- app setting -->
 		<section class="w-full flex flex-col">
 			<p class="uppercase w-full h-7 pb-3">app setting</p>
 			<div class="flex-1">
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
-				<button class="w-full h-7 text-gray-500">a</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Voice & Video</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Text & Imagess</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Appearence</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Notifications</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Keybinds</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Language</button>
+				<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Streamer Mode</button>
 			</div>
+		</section>
+
+		<div class="divide-y-4 h-3 divide-solid divide-gray-900"></div>
+
+		<!-- More -->
+
+		<section class="w-full flex flex-col">
+			<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">ChangeLog</button>
+			<button class="w-full h-7 text-gray-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Hypesquad</button>
+		</section>
+
+		<div class="divide-y-4 h-3 divide-solid divide-gray-900"></div>
+
+		<section class="w-full flex flex-col">
+			<button class="w-full h-7 text-red-500 flex items-center justify-start focus:outline-none hover:bg-gray-200">Logout</button>
 		</section>
 
 	</nav>
@@ -41,7 +63,3 @@ export default {
 
 }
 </script>
-
-<style>
-
-</style>
